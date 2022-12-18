50f606465a9047dda232524124e9215c6f0d46fd
JiazheSun <sunjz@bu.edu>
2022-12-17 23:12:31 -0500

    commit

--- static/load_and_clean_data.R
+++ static/load_and_clean_data.R
@@ -15,112 +15,98 @@ save(loan_data_clean, file = here::here("dataset/loan_refusal.RData"))
 
 ## How we clean the datasets:
 
-
+## first dataset: total greenhouse emissions in differnet sectors
 ## check if there is problems in the dataset
-air1 <- read_csv("dataset/AIRTRANS_CO2.csv", show_col_types = FALSE,
-                 col_types = cols(
-                   Time = col_character()
-                 ))
-problems(air1)
+library(here)
+library(tidyverse)
+library(dplyr)
+co2_data <- read_csv("dataset/owid-co2-data.csv", show_col_types = FALSE)
 
-## filter out the years and country range we want
-air1 <- air1 %>%  
-  filter(LOCATION == "USA", values_drop_na = TRUE) %>% 
-  filter(Time > 2009 & Time < 2020)
 
-## rearrange years to make the trend clearer
-## select out all columns we do not need
-## rename columns to make them match among datasets
-air1 <- air1 %>%
-  arrange(Time) %>% 
-  select(-POLLUTANT, -MEASURE, -FLIGHT, -FREQUENCY, -SOURCE, -SEASONALITY, -TIME, -`Flag Codes`, -Flags, -Pollutant, -LOCATION) %>%
-  rename(flight_type = `Flight type`, source_of_emissions = `Source of emissions`, Year = Time, Emission = Value, `Element(tonnes)` = Measure) %>%
-  rename(year = Year) %>%
-  select(-Frequency, -Seasonality, -flight_type) %>% filter(source_of_emissions == "Domestic aviation: (A)+(C)", source_of_emissions == "Domestic aviation (memo item): (D)+(G)")
+co2_data1 <- co2_data %>%
+  ## select variables we need to do modeling
+  select(country, year, population, gdp, energy_per_capita, energy_per_gdp, primary_energy_consumption, total_ghg) %>%
+  ## omit NAs
+  na.omit() 
 
-## reorder the variables for data combining
-col_order <- c("Country", "year", "source_of_emissions",
-                  "Emission", "Element(tonnes)")
-air1 <- air1[, col_order]
+co2_data1
+## save the cleaned dataset
+write_csv(co2_data1, file = here::here("dataset", "co2_data1_clean.csv"))
+save(co2_data1, file = here::here("dataset/co2_data1_clean.RData"))
 
-air1
-## store the data after we do all of data-cleaning
-write_csv(air1, file = here::here("dataset", "air_clean.csv"))
 
-## use kable to make dataset as table 
-knitr::kable(head(air1[, 1:5]), caption = "The CO2 emissions in airlines in United States.", "simple")
-
-#Load_data_clean
-## Check if there is problems in the dataset
+## second dataset: agriculture emissions
 Agri <- read_csv("dataset/FAOSTAT.csv", show_col_types = FALSE) 
-problems(Agri)
 
-# CLEAN the data (second dataset)
-## Filter out the country and emission element we want
-Agri2 <- 
-  Agri %>% filter(Area == "United States of America") %>%
+Agri_land <- 
+  Agri %>%
+  ## find out items related to agriculture
+  filter(str_detect(Item,"Emissions on agricultural land")) %>%
+  ## sort out emissions related to CO2 emissions
   filter(str_detect(Element, "CO2eq")) %>%
-  ## Select the years and the columns we need
+  ## make sure the unit is same for both datasets
   mutate(unit = "tonnes") %>%
-  select(Area, Item, Element, unit, Y2010:Y2019) %>%
-  pivot_longer(c(Y2010:Y2019),
+  ## select out columns we need
+  select(Area, Item, Element, unit, Y1990:Y2018) %>%
+  ## make the variable year to one column
+  pivot_longer(c(Y1990:Y2018),
                names_to = "year", values_to = "emission") %>%
-  ## Delete unnecessary columns and arrange the data in a clear version           
+  ## create a new column with new emissions
   mutate(total = emission * 1000) %>%
+  ## delete the unnecessary column
   select(-emission) %>%
+  ## arrange by year
   arrange(order(year)) %>%
+  ## group by variables
   group_by(year, Item, Area) %>%
+  ## calculate the total emissions by the group
   summarize(sum_emission = sum(total, na.rm = TRUE, count = n()), Element) %>%
-  ## Rename the columns in order to match with other two datasets
-  ## Do the final cleaning
+  ## rename all of columns for combining
   rename(Country = Area, Emission = sum_emission, source_of_emissions = Item, `Element(tonnes)` = Element) %>%
+  ## remove 'Y' in column year
   mutate(year = gsub("Y", "", year)) %>%
-  select(-Country)
-Agri2
-## Store the data after we do all of data-cleaning
-write_csv(Agri2, file = here::here("dataset", "Agri_clean.csv"))
+  select(-Country) %>%
+  ungroup() %>%
+  select(-`Element(tonnes)`)%>%
+  ## calculate total emissions
+  group_by(Country, year, source_of_emissions) %>%
+  summarise(Emissions = sum(Emission)) %>%
+  rename(country = Country) %>%
+  ## make sure names of countries are same to avoid NAs
+  mutate(country = gsub("United Kingdom of Great Britain and Northern Ireland", "United Kingdom", country)) %>%
+  mutate(country = gsub("United States of America", "United States", country)) %>%
+  mutate(country = gsub("Russian Federation", "Russia", country)) %>%
+  mutate(country = gsub("Bolivia (Plurinational State of)", "Bolivia", country)) %>%
+  mutate(country = gsub("Republic of Moldova", "Moldova", country)) %>%
+  mutate(country = gsub("Lao People's Democratic Republic", "Laos", country)) %>%
+  mutate(country = gsub("Syrian Arab Republic", "Syria", country)) %>%
+  mutate(country = gsub("United Republic of Tanzania", "Tanzania", country)) %>%
+  mutate(country = gsub("Democratic Republic of the Congo", "Democratic Republic of Congo", country)) %>%
+  mutate(country = gsub("Iran (Islamic Republic of)", "Iran", country))
+Agri_land$year = as.numeric(Agri_land$year)
+Agri_land
+## save the cleaned dataset
+write_csv(Agri_land, file = here::here("dataset", "Agri_land_clean.csv"))
+save(Agri_land, file = here::here("dataset/Agri_land_clean.RData"))
 
-## Combine first two datasets
-Agri_air <- rbind(Agri2, air1)
-Agri_air
-write_csv(Agri_air, file = here::here("dataset", "Agri_air_clean.csv"))
+## combine two datasets
 
-## third dataset:
+co2_agri <- full_join(co2_data1, Agri_land, by = c("year", "country"))
 
-## check if there is problems in the dataset:
-vehi <- suppressMessages(read_csv("dataset/Vehicle.csv", show_col_types = FALSE, skip = 1,
-                                  col_types = cols(
-                                    `2010` = col_character(),
-                                    `2014` = col_character(),
-                                    `2011` = col_character(),
-                                    `2012` = col_character(),
-                                    `2013` = col_character()
-                                  )))
+co2_agri <- co2_agri %>% 
+  ## make sure the unit is identical
+  mutate(total_ghg2 = total_ghg * 1000) %>%
+  select(-total_ghg) %>%
+  filter(country != "World") %>%
+  ## make sure the unit is identical
+  rename(`Agriculture_emissions(kilotonnes)` = Emissions, `total_ghg(kilotonnes)` = total_ghg2) %>%
+  mutate(`Agriculture_emissions(Kilotonnes)`= `Agriculture_emissions(kilotonnes)`/1000) %>%
+  select(-`Agriculture_emissions(kilotonnes)`)
+  ## omit NAs are omitted
+co2_agri <- co2_agri %>% mutate(agri_ghg_total = rowSums(co2_agri[,c(9,10)])) %>%
+  na.omit()
 
-problems(vehi)
+co2_agri
 
-## filter out some types of vehicles we do not focus on
-## assign a new name to a column that didn't have a name
-vehi1 <- vehi %>%
-  filter(!row_number() %in% c(23,24,27:95)) %>%
-  rename(source_of_emissions = ...1)
-
-## select out the years we want to do research
-## turn variable year as the column 
-vehi1 <- vehi1 %>% 
-  select(source_of_emissions, "2010":"2019") %>%
-  pivot_longer(c("2010":"2019"), names_to = "year")
-
-## remove all of NAs
-## save the data after data-cleaning
-vehi1 <- vehi1 %>% 
-  na.omit(vehi) %>% 
-  rename(type_of_cars = source_of_emissions)
-vehi1
-write_csv(vehi1, file = here::here("dataset", "vehi_clean.csv"))
-
-## Combine three datasets
-Agri_air_car <- left_join(Agri_air, vehi1, by = "year")
-Agri_air_car
-knitr::kable(head(Agri2[, 1:5]), caption = "The CO2 emissions in combined airlines, agriculture, and vehicles in United States.", "simple")
-write_csv(Agri_air_car, file = here::here("dataset", "Agri_air_clean.csv"))
+write_csv(co2_agri, file = here::here("dataset", "co2_agri_clean.csv"))
+save(co2_agri, file = here::here("dataset/co2_agri_clean.RData"))
